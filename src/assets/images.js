e9afb5dd0451392f5dc4e742076022b9273df755
student izaz <izazahmed109@gmail.com>
2025-07-08 19:39:56 +0530

    work.jsx

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,5 @@
 import my_img from "./my_img.jpg"
-import grocery_img from "./grocery_img.png";
+import begumsaab from "./begumsaab.png"
 import techpile_img from "./techpile_img.png";
 import facebook_img from "./facebook_img.png";
 import youtube_img from "./youtube_img.png";
@@ -8,7 +8,7 @@ import sql_certificate from "./sql_certificate.png";
 
 export const assets = { 
     my_img,
-    grocery_img,
+    begumsaab,
     techpile_img,
     facebook_img,
     youtube_img,
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -18,10 +18,10 @@ function Work() {
           <div className="work-area flex rg-4 flex-wrap">
             <div className="work-sample flex-col rg-2">
               <div className="work-img">
-                <img src={assets.grocery_img} alt="Work_Img" />
+                <img src={assets.begumsaab} alt="Work_Img" />
               </div>
               <div className="about-work">
-                <div className="work-title"><h3>MERN Grocery App</h3></div>
+                <div className="work-title"><h3>MERN Online Suits Booking App</h3></div>
                 <div className="work-desc">
                   <p>E-commerce website with features like product listing, category filtering, user authentication, shopping cart.</p>
                 </div>
@@ -32,7 +32,8 @@ function Work() {
                   <div className="tech">Redux</div>
                 </div>
                 <div className="work-link">
-                  <a href="#"><FaGithub /> View on Github</a>
+                  <a href="https://github.com/student-izaz/begumsaab.git"><FaGithub /> View on Github</a>
+                  <a href="https://begumsaab.vercel.app/"><FaGithub /> Live Preview</a>
                 </div>
               </div>
             </div>
