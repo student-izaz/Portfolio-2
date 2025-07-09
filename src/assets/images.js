05278db67ef9aaa6c70cdf8b853ecd2290fdfdb3
student izaz <izazahmed109@gmail.com>
2025-07-09 21:39:19 +0530

    add work finance tracker

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,9 +1,9 @@
 import my_img from "./my_img.jpg"
 import begumsaab from "./begumsaab.png"
 import skillswapp from "./skillswapp.png";
-import finance_tracker from "./finance_tracker.png"
 import Deloitte_certificate from "./Deloitte_Certificate.png";
 import sql_certificate from "./sql_certificate.png";
+import finance_tracker from "./finance_tracker.png"
 
 export const assets = { 
     my_img,
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,7 +16,7 @@ function Work() {
         </div>
         <div className="work-container">
           <div className="work-area flex rg-4 flex-wrap">
-            <div className="work-sample flex-col rg-2">
+            {/* <div className="work-sample flex-col rg-2">
               <div className="work-img">
                 <img src={assets.finance_tracker} alt="Work_Img" />
               </div>
@@ -36,9 +36,9 @@ function Work() {
                   <a href="https://finance-tracker-eight-drab.vercel.app//"><FaGithub /> Live Preview</a>
                 </div>
               </div>
-            </div>
+            </div> 
 
-            <div className="work-sample flex-col rg-2">
+            {/* <div className="work-sample flex-col rg-2">
               <div className="work-img">
                 <img src={assets.begumsaab} alt="Work_Img" />
               </div>
@@ -58,7 +58,7 @@ function Work() {
                   <a href="https://begumsaab.vercel.app/"><FaGithub /> Live Preview</a>
                 </div>
               </div>
-            </div>
+            </div> */}
 
             <div className="work-sample flex-col rg-2">
               <div className="work-img">
@@ -75,7 +75,50 @@ function Work() {
                   <div className="tech">Express js</div>
                 </div>
                 <div className="work-link">
-                  <a href="#"><FaGithub /> View on Github</a>
+                  <a href="https://github.com/student-izaz/skillswapp.git"><FaGithub /> View on Github</a>
+                  {/* <a href="https://begumsaab.vercel.app/"><FaGithub /> Live Preview</a> */}
+                </div>
+              </div>
+            </div>
+
+            <div className="work-sample flex-col rg-2">
+              <div className="work-img">
+                <img src={assets.finance_tracker} alt="Work_Img" />
+              </div>
+              <div className="about-work">
+                <div className="work-title"><h3>Finance Tracker MERN</h3></div>
+                <div className="work-desc">
+                  <p>The Finance Tracker is a full-stack web application built using the MERN stack...</p>
+                </div>
+                <div className="work-tech">
+                  <div className="tech">React</div>
+                  <div className="tech">Node js</div>
+                  <div className="tech">Express js</div>
+                </div>
+                <div className="work-link">
+                  <a href="https://github.com/student-izaz/Project-Finance-Tracker.git"><FaGithub /> View on Github</a>
+                  <a href="https://finance-tracker-eight-drab.vercel.app//"><FaGithub /> Live Preview</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="work-sample flex-col rg-2">
+              <div className="work-img">
+                <img src={assets.begumsaab} alt="Work_Img" />
+              </div>
+              <div className="about-work">
+                <div className="work-title"><h3>Ecommerce MERN</h3></div>
+                <div className="work-desc">
+                  <p>E-commerce website with features like product listing, category filtering, user authentication, shopping cart.</p>
+                </div>
+                <div className="work-tech">
+                  <div className="tech">React</div>
+                  <div className="tech">Node js</div>
+                  <div className="tech">Express js</div>
+                </div>
+                <div className="work-link">
+                  <a href="https://github.com/student-izaz/begumsaab.git"><FaGithub /> View on Github</a>
+                  <a href="https://begumsaab.vercel.app/"><FaGithub /> Live Preview</a>
                 </div>
               </div>
             </div>
